e1bbffd119a15a3857fd14b4ff184fa8b42433c9
Blair Currey <curreyb88@gmail.com>
2021-04-07 23:15:05 -0400

    renamed dir to model

--- server/database/schemas/Animal.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const animalSchema = new mongoose.Schema({
-    type: { type: String, lowercase: true, required: true, unique: true, immutable: true },
-  });
-
-const Animal = mongoose.model('Animal', animalSchema);
-
-module.exports = Animal;
--- server/database/schemas/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./User');
-const Animal = require('./Animal');
-const Sighting = require('./Sighting');
-
-module.exports = {
-  User,
-  Animal,
-  Sighting
-};
