9c01dfbc10f71b6fbf3b0bf454a50da59422eb4e
Blair Currey <curreyb88@gmail.com>
2021-04-22 00:32:19 -0400

    added border to map

--- client/src/components/Home/Map.js
+++ client/src/components/Home/Map.js
@@ -6,9 +6,10 @@ function Map({sightings}){
             center={[0, 0]} 
             zoom={1} 
             scrollWheelZoom={true}
-            style={{height: "360px"}}
+            style={{height: "360px", border: "1px solid gray"}}
         >
             <TileLayer
+                // noWrap={true}
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
