573e0dcc96b4856c6b43e228d2ac6192c11a4a1c
Blair Currey <curreyb88@gmail.com>
2021-05-11 19:43:03 -0400

    converted sighting list to mui datagrid

--- client/src/components/Home/Sightings.js
+++ client/src/components/Home/Sightings.js
@@ -1,25 +1,60 @@
-import React from "react";
-import { CircularProgress } from "@material-ui/core"
+import React, { useState, useEffect } from "react";
+import { CircularProgress } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Sightings({sightings}){
+const useStyles = makeStyles((theme) => ({
+    tableWrapper: { height: 400, width: "100%" },
+}));
+
+const columns = [
+    { field: "date", headerName: "Date", width: 125 },
+    {
+        field: "animal",
+        headerName: "Animal",
+        width: 125,
+    },
+    { field: "coordinates", headerName: "Coordinates", width: 175 },
+    { field: "user", headerName: "User", width: 100 },
+    { field: "dateSubmitted", headerName: "Date Submitted", width: 175 },
+];
+
+function Sightings({ sightings }) {
+    const makeRows = () => {
+        let r = [];
+        sightings.forEach((s) => {
+            r.push({
+                id: s._id,
+                date: s.date.split("T")[0],
+                animal: s.animal.type,
+                coordinates: `${s.location.latitude.toFixed(
+                    4
+                )}, ${s.location.longitude.toFixed(4)}`,
+                user: s.user.username,
+                dateSubmitted: s.createdAt.split("T")[0],
+            });
+        });
+        return r;
+    };
+
+    const classes = useStyles();
+
     return (
-    <div>
-        <h2>Sightings</h2>
-        {!sightings 
-            ? <CircularProgress />
-            : sightings.map(s => (
-                <ul key={`${s._id}`}>
-                    <li key={`${s._id}-username`}>User: {s.user.username}</li>
-                    <li key={`${s._id}-date`}>Date: {s.date}</li>
-                    <li key={`${s._id}-animal`}>Animal: {s.animal.type}</li>
-                    <li key={`${s._id}-coordinates`}>Coordinates: {s.location.latitude}, {s.location.longitude}</li>
-                    <li key={`${s._id}-comment`}>Comment: {s.comment}</li>
-                    <li key={`${s._id}-submitted`}>Submitted: {s.createdAt}</li>
-                </ul>
-            ))
-        }
-    </div>
-    )
+        <div>
+            <h2>Sightings</h2>
+            {!sightings ? (
+                <CircularProgress />
+            ) : (
+                <div className={classes.tableWrapper}>
+                    <DataGrid
+                        rows={makeRows()}
+                        columns={columns}
+                        pageSize={5}
+                    ></DataGrid>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Sightings;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,90 +1,92 @@
-import { React, useState, useEffect } from 'react';
-import { Box, Paper } from '@material-ui/core';
+import { React, useState, useEffect } from "react";
+import { Box, Paper } from "@material-ui/core";
 
-import SightingFormWrapper from './SightingFormWrapper.js'
-import Sightings from './Sightings.js';
-import Map from './Map.js';
-import { 
-  animalIdValidator,
-  latitudeValidator,
-  longitudeValidator,
-  dateValidator,
-  commentValidator
-} from '../../utils/validators';
+import SightingFormWrapper from "./SightingFormWrapper.js";
+import Sightings from "./Sightings.js";
+import Map from "./Map.js";
+import {
+    animalIdValidator,
+    latitudeValidator,
+    longitudeValidator,
+    dateValidator,
+    commentValidator,
+} from "../../utils/validators";
 
-function Home(props){
-  const [result, setResult] = useState();
-  const [animals, setAnimals] = useState([]);
-  const [sightings, setSightings] = useState([]);
+function Home(props) {
+    const [result, setResult] = useState();
+    const [animals, setAnimals] = useState([]);
+    const [sightings, setSightings] = useState([]);
+    const [rows, setRows] = useState([]);
 
-  const  validate = {
-    animalId: animalIdValidator,
-    latitude: latitudeValidator,
-    longitude: longitudeValidator,
-    date: dateValidator,
-    comment: commentValidator
-}
-  const initialValues = {
-    animalId: "",
-    latitude: "",
-    longitude: "",
-    date: "",
-    comment: ""
-  }
+    const validate = {
+        animalId: animalIdValidator,
+        latitude: latitudeValidator,
+        longitude: longitudeValidator,
+        date: dateValidator,
+        comment: commentValidator,
+    };
+    const initialValues = {
+        animalId: "",
+        latitude: "",
+        longitude: "",
+        date: "",
+        comment: "",
+    };
 
-  const fetchAnimals = async () => {
-    const response = await fetch(`/api/animals`);
-    const data = await response.json();
-    setAnimals(data.animals);
-  }
+    const fetchAnimals = async () => {
+        const response = await fetch(`/api/animals`);
+        const data = await response.json();
+        setAnimals(data.animals);
+    };
 
-  const fetchSightings = async () => {
-    const response = await fetch(`/api/sightings`);
-    const data = await response.json();
-    setSightings(data.sightings);
-  }
+    const fetchSightings = async () => {
+        const response = await fetch(`/api/sightings`);
+        const data = await response.json();
+        setSightings(data.sightings);
+    };
 
-  useEffect(() => {
-    fetchAnimals();
-    fetchSightings();
-  }, []);
+    useEffect(() => {
+        fetchAnimals();
+        fetchSightings();
+    }, []);
 
-  const requestParams = data => {
-    return ({
-        url: '/api/sightings',
-        options: {
-            method: 'POST',
-            headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+    const requestParams = (data) => {
+        return {
+            url: "/api/sightings",
+            options: {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
             },
-            body: JSON.stringify(data)
-        }
-    })
-}
-  const setResponse = async requestResult => {
-    let data = await requestResult.json();
-    setResult(data.message)
-  };
-  return (
-    <Box my={5}>
-      <Box my={1}>
-        <Paper >
-          <SightingFormWrapper 
-            animals={animals}
-            fetchSightings={fetchSightings}
-            validate={validate}
-            initialValues={initialValues}
-            requestParams={requestParams}
-            setResponse={setResponse}
-            result={result}
-          />
-        </Paper>
-      </Box>
-      <Map sightings={sightings}/>
-      <Sightings sightings={sightings}/>
-    </Box>
-  );
+        };
+    };
+    const setResponse = async (requestResult) => {
+        let data = await requestResult.json();
+        setResult(data.message);
+    };
+
+    return (
+        <Box my={5}>
+            <Box my={1}>
+                <Paper>
+                    <SightingFormWrapper
+                        animals={animals}
+                        fetchSightings={fetchSightings}
+                        validate={validate}
+                        initialValues={initialValues}
+                        requestParams={requestParams}
+                        setResponse={setResponse}
+                        result={result}
+                    />
+                </Paper>
+            </Box>
+            <Map sightings={sightings} />
+            <Sightings sightings={sightings} />
+        </Box>
+    );
 }
 
 export default Home;
